2fc5ef1b96ea66659873025c34c5e2c391494259
AndrewLevada <zipeofficial@gmail.com>
2020-06-02 18:25:57 +0300

    config and secret key removed from .gitignore

---
+++ src/main/java/com/andrewlevada/carephoneserver/Config.java
@@ -0,0 +1,6 @@
+package com.andrewlevada.carephoneserver;
+
+public class Config {
+    public static final String serviceAccountKeyPath = "firebase-service-account.json";
+    public static final String firebaseDatabaseURL = "https://carephone-app.firebaseio.com";
+}
