416df138c23f40bd8e00130e4ce9a356c7635e55
AndrewLevada <zipeofficial@gmail.com>
2020-06-02 12:20:14 +0300

    PhoneNumber logic added

--- src/main/java/com/andrewlevada/carephoneserver/Requests.java
+++ src/main/java/com/andrewlevada/carephoneserver/Requests.java
@@ -1,10 +1,14 @@
 package com.andrewlevada.carephoneserver;
 
+import com.andrewlevada.carephoneserver.logic.PhoneNumber;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @EnableAutoConfiguration
@@ -12,8 +16,12 @@ public class Requests {
     @Autowired
     private Repository repository;
 
-    @RequestMapping(path="/getCared/{token}")
-    public String getCared(@PathVariable String token) {
-        return "test";
+    @RequestMapping(path="/whitelist/{userToken}")
+    public List<PhoneNumber> getCared(@PathVariable String userToken) {
+        List<PhoneNumber> array = new ArrayList<>();
+        array.add(new PhoneNumber("+7894", "Person"));
+        array.add(new PhoneNumber("+78740224", "Hello"));
+        array.add(new PhoneNumber("+799999894", "Third"));
+        return array;
     }
 }
---
+++ src/main/java/com/andrewlevada/carephoneserver/logic/PhoneNumber.java
@@ -0,0 +1,11 @@
+package com.andrewlevada.carephoneserver.logic;
+
+public class PhoneNumber {
+    public String phone;
+    public String label;
+
+    public PhoneNumber(String phone, String label) {
+        this.phone = phone;
+        this.label = label;
+    }
+}
